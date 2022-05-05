2b658d9ee4cafb609e7ac7c7e3126e6c22bda535
Danil <drazdobarov.job@yandex.ru>
2022-05-05 13:37:01 +1000

    Created a project, created a navigation for the fragments, and initialized the links between them

---
+++ app/src/main/java/com/mimoza_app/notes/radzze/screens/AddNewNoteFragment.kt
@@ -0,0 +1,19 @@
+package com.mimoza_app.notes.radzze.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mimoza_app.notes.radzze.R
+
+class AddNewNoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_new_note2, container, false)
+    }
+}
---
+++ app/src/main/java/com/mimoza_app/notes/radzze/screens/MainFragment.kt
@@ -0,0 +1,20 @@
+package com.mimoza_app.notes.radzze.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mimoza_app.notes.radzze.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
